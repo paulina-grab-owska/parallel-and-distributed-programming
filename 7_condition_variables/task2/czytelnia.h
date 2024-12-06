a55cc9315ba8d17536262d1aead144ec48b6c2cc
Paulina Grabowska <165821947+paulina-grab-owska@users.noreply.github.com>
2024-12-06 11:48:04 +0100

    Create czytelnia.h

---
+++ 7_condition_variables/task2/czytelnia.h
@@ -0,0 +1,20 @@
+#ifndef _czytelnia_
+#define _czytelnia_
+
+/*** Definicje typow zmiennych ***/
+typedef struct {
+  int l_p; // liczba piszacych
+  int l_c; // liczba czytajacych
+} cz_t;
+
+/*** Deklaracje procedur interfejsu ***/
+void inicjuj(cz_t* czytelnia_p);
+void czytam(cz_t* czytelnia_p);
+void pisze(cz_t* czytelnia_p);
+
+int my_read_lock_lock(cz_t* czytelnia_p);
+int my_read_lock_unlock(cz_t* czytelnia_p);
+int my_write_lock_lock(cz_t* czytelnia_p);
+int my_write_lock_unlock(cz_t* czytelnia_p);
+
+#endif
